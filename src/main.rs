42c52cadee09c0327d203a828481507098eb1455
akshitkrnagpal <akshitkrnagpal@gmail.com>
2018-10-30 14:07:06 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+struct Fibonacci {
+    curr: u64,
+    next: u64,
+}
+
+impl Fibonacci {
+    fn new ()-> Self {
+        Fibonacci { curr: 1, next: 1 }
+    }
+}
+
+impl Iterator for Fibonacci {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        let new_next = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new_next;
+
+        Some(self.curr)
+    }
+}
+
+fn main() {
+    for i in Fibonacci::new().take(50) {
+        println!("{}", i);
+    }
+}
